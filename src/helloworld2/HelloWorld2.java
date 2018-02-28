b88c528588f3452dba9846f566d48b8af81f4ad4
xagod6351 <xagod6351@7KXN6Y1.Student.UGDSB.ED>
2018-02-28 13:23:22 -0500

    First time ;)

---
+++ src/helloworld2/HelloWorld2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld2;
+
+/**
+ *
+ * @author xagod6351
+ */
+public class HelloWorld2 {
+    int bus;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Omega LUL and an arm");
+        
+    }
+    
+}
